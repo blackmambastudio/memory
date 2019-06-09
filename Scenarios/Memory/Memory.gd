5bbf8d716938f0e5cf391e32e3d0dd0a420c7b31
Agar3s <ekeisco@gmail.com>
2019-06-09 01:37:27 -0500

    basic masking refactor

--- Scenarios/Memory/Memory.gd
+++ Scenarios/Memory/Memory.gd
@@ -9,10 +9,11 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
-		$Light2D.position.x -= speed*delta
+		$PortionLight.position.x -= speed*delta
 	if Input.is_action_pressed("ui_right"):
-		$Light2D.position.x += speed*delta
+		$PortionLight.position.x += speed*delta
 	if Input.is_action_pressed("ui_up"):
-		$Light2D.position.y -= speed*delta
+		$PortionLight.position.y -= speed*delta
 	if Input.is_action_pressed("ui_down"):
-		$Light2D.position.y += speed*delta
+		$PortionLight.position.y += speed*delta
+	
