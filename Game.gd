ae994c03906f339ec219738ff757cf921ad55961
Carenalga <mapedorr@gmail.com>
2019-06-17 19:48:39 -0500

    Added timeouts to first dialog.
    Changed the mouse cursor.
    Centered the dialogs text.
    Created animation to show/hide the memories.
    Ready to work on dialogs for M01.

--- Game.gd
+++ Game.gd
@@ -1,6 +1,29 @@
 extends Control
 
+onready var arrow = load('res://Assets/Cursor/Arrow.png')
+onready var pointing_hand = load('res://Assets/Cursor/PointingHand.png')
+onready var ActionRouter = get_node("/root/ActionRouter")
+var click_pressed = false
 
 func _ready():
+	# Setup the mouse cursor for the game
+	Input.set_custom_mouse_cursor(arrow, Input.CURSOR_ARROW)
+	Input.set_custom_mouse_cursor(
+		pointing_hand,
+		Input.CURSOR_POINTING_HAND,
+		Vector2(6.0, 0.0)
+	)
+
+	# Setup signal listeners
 	$Memory/Canvas.connect("item_selected", $DialogueManager, "_on_item_selected")
 
+#func _process(delta):
+#	if not click_pressed and not $Memory.open \
+#		and  Input.is_mouse_button_pressed(BUTTON_LEFT):
+#		print("NEXT!")
+#		click_pressed = true
+#		ActionRouter.request({
+#			"action": "Dialogue/next"
+#		})
+#		yield(get_tree().create_timer(0.1), "timeout")
+#		click_pressed = false
--- Scenarios/Memory/Memory.gd
+++ Scenarios/Memory/Memory.gd
@@ -1,11 +1,11 @@
 extends Control
 
 var speed = 200
+var open = false
 
 func _ready():
 	var ActionRouter = get_node("/root/ActionRouter")
 	ActionRouter.register_actions(self)
-	self.hide()
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
@@ -20,4 +20,9 @@ func _process(delta):
 func handle(request):
 	match request.action:
 		"Memory/show":
-			self.show()
+			open = request.value
+			$AnimationPlayer.play(
+				"Show",
+				-1, 1.0 if request.value else -1.0,
+				!request.value
+			)
