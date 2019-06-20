2c9c23ab9dca4d3710dd54101953f24c4c33bbd3
Agar3s <ekeisco@gmail.com>
2019-06-20 11:03:34 -0500

    load words into the soup

--- Scenarios/Minigames/SopaLetras/SopaLetras.gd
+++ Scenarios/Minigames/SopaLetras/SopaLetras.gd
@@ -2,13 +2,19 @@ tool
 extends Node2D
 
 const Letter = preload("res://Scenarios/Minigames/SopaLetras/Letter.tscn")
-export (String) var text = 'detodaslasletrasparecenalgunasquenosiempresoncorrectasytocaadivinar'
+signal word_match
 
-var words = {
-	"word1": [2,3,4,5,6,7,8,9,10],
-	"word2": [22,33,44,55,66]
-}
-var pairs = [[2,10, 'word1'], [22,66, 'word2'], [55,66, 'word3'], [33, 40, 'word4'], [66, 120, 'word5']]
+export (Array) var words = [
+	{"word":"remember", "col": 2, "row": 2, "direction": 2},
+	{"word":"salvation", "col": 0, "row": 0, "direction": 4},
+	{"word":"future", "col": 2, "row": 5, "direction": 4},
+	{"word":"flower", "col": 2, "row": 5, "direction": 3},
+	{"word":"past", "col": 9, "row": 4, "direction": 6},
+	{"word":"peace", "col": 9, "row": 4, "direction": 4},
+]
+
+var word_indexes = {}
+var pairs = []
 
 var initiaded = false
 var one_pair = -1
@@ -17,6 +23,8 @@ var hovered
 func _ready():
 	var limit = 12*12
 	var index = 0
+	var text = populate()
+	
 	for character in text:
 		var letra = Letter.instance()
 		letra.set_letter(index, character)
@@ -26,6 +34,7 @@ func _ready():
 		index+=1
 		if index>=limit:
 			break
+	insert_words()
 
 func letter_clicked(index):
 	if index == one_pair: return
@@ -41,13 +50,13 @@ func letter_clicked(index):
 		for pair in pairs:
 			if (pair[0] == one_pair or pair[0] == index) and \
 			   (pair[1] == one_pair or pair[1] == index):
-				print("PAIR MATCHED!!")
-				for index_letter in words[pair[2]]:
+				emit_signal("word_match", pair[2])
+				for index_letter in word_indexes[pair[2]]:
 					$GridContainer.get_child(index_letter).set_matched()
 		
 		letter.unselect()
 		$GridContainer.get_child(one_pair).unselect()
-		index = -1
+		one_pair = -1
 	$Selector.visible = initiaded
 
 func set_hovered(letter):
@@ -57,3 +66,52 @@ func _process(delta):
 	if hovered and one_pair!=-1:
 		$Selector.points[1].x = $GridContainer.rect_position.x + hovered.rect_position.x + 15
 		$Selector.points[1].y = $GridContainer.rect_position.x + hovered.rect_position.y + 15
+
+func populate():
+	var vocals = 'aeiou'
+	var consonants = 'bcdfghjklmnpqrstvwxyz'
+	var text = ''
+	for i in range(0, 144):
+		var choice = randi()%100
+		var letter = ''
+		if choice < 40:
+			letter = vocals[randi()%vocals.length()]
+		else:
+			letter = consonants[randi()%consonants.length()]
+		text += letter
+	
+	return text
+
+
+func insert_words():
+	for w in words:
+		print(w)
+		var text = w["word"]
+		var col = w["col"]
+		var row = w["row"]
+		var direction = w["direction"]
+		
+		var _word = []
+		
+		for letter in text:
+			var index = col + row*12
+			_word.append(index)
+			$GridContainer.get_child(index).set_text(letter)
+			if direction == 7 || direction == 0 || direction == 1:
+				row -= 1
+			if direction == 3 || direction == 4 || direction == 5:
+				row += 1
+			if direction == 1 || direction == 2 || direction == 3:
+				col += 1
+			if direction == 5 || direction == 6 || direction == 7:
+				col -= 1
+
+		word_indexes[text] = _word
+		pairs.append([_word[0], _word[-1], text])
+
+
+
+
+
+
+
