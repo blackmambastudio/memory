4655b6c442b26b318ad42e8b1b1e4ac408c933fa
Carenalga <mapedorr@gmail.com>
2019-07-01 08:25:07 -0500

    Made an update in the way Act 4 ends

--- Acts/Act4/FrontRun.gd
+++ Acts/Act4/FrontRun.gd
@@ -6,7 +6,4 @@ func _ready():
 	$AnimationPlayer.connect("animation_finished", self, "change_act")
 
 func change_act(animation):
-	ActionRouter.request({
-		"action": "Act/end",
-		"next": "res://Acts/Act5/Act5.tscn"
-	})
+	pass
